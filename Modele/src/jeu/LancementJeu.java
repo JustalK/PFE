9e44e3f8fa6ba7164c7de3b44033e1c84db724ac
Latsuj <justal.kevin@gmail.com>
2015-10-21 01:34:06 +0200

    [0] #Justal Kevin - Delete the files in the temporary file when the program ends.

---
+++ Modele/src/jeu/LancementJeu.java
@@ -0,0 +1,16 @@
+package jeu;
+
+import java.io.File;
+
+/** classe pour lancer le jeu
+ * Elle crée simplement une instance de MenuJeu
+ * 
+ * @author helene
+ *
+ */
+public class LancementJeu{
+
+    public static void main(String args[]){
+			new jeu.MenuJeu("Exemple de jeu");
+    }
+}
