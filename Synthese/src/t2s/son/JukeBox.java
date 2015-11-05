9a47afd34c29cc80e490bffbe67c42976d5c460d
Latsuj <justal.kevin@gmail.com>
2015-11-05 20:00:07 +0100

    [4] Reading a wav file
    We can now read a wave file like in the old way

--- Synthese/src/t2s/son/JukeBox.java
+++ Synthese/src/t2s/son/JukeBox.java
@@ -4,36 +4,26 @@
 
 package t2s.son;
 
-import java.io.BufferedInputStream;
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.concurrent.locks.ReentrantLock;
-
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.DataLine;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.SourceDataLine;
-import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class JukeBox {
 	
-	
-	private ReentrantLock	mutex;
-	private String _fileToRead;
+	private Player player;
 	
 	public void stop(boolean stop) {
-		
 	}
 
 	// False, wait , true
-	public void playSound(String filename, boolean loop, final boolean waitUntilAudioEnds) {
+	public void playSound(String path, boolean loop, final boolean waitUntilAudioEnds) {
 		//Player p = new Player(filename, loop, waitUntilAudioEnds, eraseAudioFileAfterPlayback);
 		//playSound(p);
+		File song = new File(path); 
+		if(song.exists()) {
+			player = new Player(path);
+			player.start();
+		} else {
+			System.err.println("Le fichier suivant est introuvable :\n"+path);
+		}
 	}	
 	
 	// False, wait , true
--- Synthese/src/t2s/son/player.java
+++ Synthese/src/t2s/son/player.java
@@ -1,5 +1,26 @@
 package t2s.son;
 
-public class player extends Thread {
+import java.applet.Applet;
+import java.applet.AudioClip;
+import java.net.MalformedURLException;
+import java.net.URL;
 
+public class Player extends Thread {
+
+	private AudioClip ac;
+	
+	public Player(String path) {
+		try {
+			URL u = new URL("file:"  + path);
+			ac = Applet.newAudioClip(u);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void run() {
+		System.out.println("azezaeazezae");
+		ac.play();
+	}
 }
